01a6c5a794c06e6a4e5dcbe9e15c1bbd81e2c8d5
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-11 17:12:03 -0300

    Update

--- register.php
+++ register.php
@@ -41,6 +41,20 @@ if(isset($_POST['submit'])){
 </head>
 <body>
 
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+
     <!-- ======= Formulario de Registro ======= -->
     <div class="form-container">
         <form action="" meehod="post">
