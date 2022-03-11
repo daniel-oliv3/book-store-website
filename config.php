775c12a0bdb7e9a4eac98b683df65814516e707f
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-11 16:51:58 -0300

    Update

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php 
+
+$conn = mysqli_connect('localhost','root','','book-store-db') or die('connection failed');
+
+
+?>
--- register.php
+++ register.php
@@ -1,3 +1,32 @@
+<?php
+
+include 'config.php';
+
+if(isset($_POST['submit'])){
+
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+    $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+    $user_type = $_POST['user_type'];
+ 
+    $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+ 
+    if(mysqli_num_rows($select_users) > 0){
+       $message[] = 'o utilizador já existe!';
+    }else{
+       if($pass != $cpass){
+          $message[] = 'confirme a senha não correspondida!';
+       }else{
+          mysqli_query($conn, "INSERT INTO `users`(name, email, password, user_type) VALUES('$name', '$email', '$cpass', '$user_type')") or die('query failed');
+          $message[] = 'Registrado com sucesso!';
+          header('location:login.php');
+       }
+    }
+ 
+ }
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
